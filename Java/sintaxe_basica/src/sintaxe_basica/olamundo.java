531d45fe25ae14e365ca523c289b79f5095eca91
Gleyson Moura <gleyson.sistinfo@gmail.com>
2022-11-03 11:04:55 -0300

    arquivos_upload_inicial

---
+++ Java/sintaxe_basica/src/sintaxe_basica/olamundo.java
@@ -0,0 +1,10 @@
+package sintaxe_basica;
+
+public class olamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ola mundo");
+	}
+
+}
---
+++ Java/sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/testa_variaveis.java
@@ -0,0 +1,25 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class testa_variaveis {
+	
+	public static void main (String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+
+		idade = 36;
+		System.out.println("minha idade é: " + idade);
+		
+		idade += 10;
+		System.out.println("minha idade é: " + idade);
+		
+		idade = 26 + 10;
+		System.out.println("minha idade é: " + idade);
+		
+		idade = (7 * 5) + 2;
+		System.out.println("minha idade é: " + idade);
+		
+		
+	}
+		
+}
